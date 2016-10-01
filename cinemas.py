2daa26374c30c69fcc62f353f1cd49f636395d7e
Melevir <melevir@gmail.com>
2016-10-01 12:10:38 +0300

    added boilerplate

---
+++ cinemas.py
@@ -0,0 +1,20 @@
+
+
+def fetch_afisha_page():
+    pass
+
+
+def parse_afisha_list(raw_html):
+    pass
+
+
+def fetch_movie_info(movie_title):
+    pass
+
+
+def output_movies_to_console(movies):
+    pass
+
+
+if __name__ == '__main__':
+    pass
