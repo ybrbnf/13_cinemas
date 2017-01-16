f83cf7ef5aa63336d15542be7e06eba52a6e75a4
Nikita Tkachenko <ybrbnf418@gmail.com>
2017-01-16 14:04:14 +0400

    fixed various bugs

--- cinemas.py
+++ cinemas.py
@@ -5,27 +5,42 @@ import argparse
 import time
 
 
+def get_args():
+    parser = argparse.ArgumentParser(
+        description='Shows top films sorted by rating www.kinopoisk.ru'
+         )
+    parser.add_argument('-c', '--cinemas',
+                        help='Shows top films sorted by the number of cinemas',
+                        action='store_true'
+                        )
+    args = parser.parse_args()
+    return args
+
+
 def fetch_afisha_page():
     afisha_url = 'http://www.afisha.ru/msk/schedule_cinema/'
     return requests.get(afisha_url)
 
 
 def parse_afisha_list(afisha_response):
-    film_titles = []
-    num_of_cinemas = {}
+    base_film_info = []
     soup = BeautifulSoup(afisha_response.text, 'lxml')
-    film_div = soup.findAll('div', {
-     'class': 'object s-votes-hover-area collapsed'
-    }
+    film_div = soup.findAll(
+        'div',
+        {'class': 'object s-votes-hover-area collapsed'}
     )
     for item in film_div:
         for element in item.findAll('h3', {'class': 'usetags'}):
-            film_titles.append(element.text)
-            num_of_cinemas[element.text] = len(item.findAll('td',
-                                               {'class': 'b-td-item'}
+            base_film_info.append({
+                'film_title': element.text,
+                'cinemas': len(item.findAll(
+                    'td',
+                    {'class': 'b-td-item'}
+                )
+                )
+            }
             )
-            )
-    return film_titles, num_of_cinemas
+    return base_film_info
 
 
 def search_film(film_title):
@@ -42,28 +57,42 @@ def search_film(film_title):
     return search_response
 
 
-def get_films_rating_and_voters(film_titles):
-    sleep_time = 1
-    rating = {}
-    voters = {}
-    for film_title in film_titles:
+def get_films_info_from_kp(base_film_info):
+    sleep_time = 5
+    full_film_info = []
+    for item in base_film_info:
+        film_title = item['film_title']
         search_response = search_film(film_title)
         soup = BeautifulSoup(search_response.text, 'lxml')
         rating_count = soup.find('span', {'class': 'rating_ball'})
         voters_count = soup.find('span', {'class': 'ratingCount'})
-        if rating_count is not None:
-            rating[film_title] = rating_count.text
-        if voters_count is not None:
-            voters[film_title] = voters_count.text
+        if rating_count is None:
+            rating = None
+        else:
+            rating = rating_count.text
+        if voters_count is None:
+            voters = None
+        else:
+            voters = int(voters_count.text.replace('\xa0', ''))
+        full_film_info.append({
+            'film_title': item['film_title'],
+            'cinemas': item['cinemas'],
+            'rating': rating,
+            'voters': voters
+        }
+        )
         time.sleep(sleep_time)
-    return rating, voters
+    return full_film_info
 
 
-def get_sorted_films(rating, num_of_cinemas):
+def get_sorted_films(full_film_info):
     if args.cinemas:
-        sorted_films = sorted(num_of_cinemas.items(), key=lambda x: x[1])
+        sorted_films = sorted(full_film_info, key=lambda i: i['cinemas'])
     else:
-        sorted_films = sorted(rating.items(), key=lambda x: x[1])
+        sorted_films = sorted(full_film_info,
+                              key=lambda i:
+                              (i['rating'] is not None, i['rating'])
+                              )
     sorted_films.reverse()
     return sorted_films
 
@@ -71,22 +100,17 @@ def get_sorted_films(rating, num_of_cinemas):
 def output_top_films_to_console(sorted_films):
     top_films = 10
     if args.cinemas:
-        print('TOP фильмов, сортированных по количеству мест показа:')
+        print('Top films sorted by the number of cinemas')
     else:
-        print('TOP фильмов, сортированных по рейтингу КИНОПОИСКа:')
+        print('Top films sorted by rating www.kinopoisk.ru:')
     for item in sorted_films[:top_films]:
-      print(item[0])
+        print(item['film_title'])
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(add_help=False)
-    parser.add_argument('-h', '--help', action='help',
-                        help='Справкв')
-    parser.add_argument('-c', '--cinemas', action="store_true",
-                        help='Сортировка по количеству кинотеатров')
-    args = parser.parse_args()
+    args = get_args()
     afisha_response = fetch_afisha_page()
-    film_titles, num_of_cinemas = parse_afisha_list(afisha_response)
-    rating, voters = get_films_rating_and_voters(film_titles)
-    sorted_films = get_sorted_films(rating, num_of_cinemas)
+    base_film_info = parse_afisha_list(afisha_response)
+    full_film_info = get_films_info_from_kp(base_film_info)
+    sorted_films = get_sorted_films(full_film_info)
     output_top_films_to_console(sorted_films)
